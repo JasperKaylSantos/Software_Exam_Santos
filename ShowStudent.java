48dcea259f899767a701bf8faedb165b0c528bb3
JasperSantos <JasperKaylSantos@users.noreply.github.com>
2021-03-16 16:33:40 +0800

    2nd commit ShowStudents

---
+++ ShowStudent.java
@@ -0,0 +1,18 @@
+package tutorial;
+import java.util.Scanner;
+
+
+public class showstudent2 {
+	public static void main(String[] args) {
+	       Scanner sc = new Scanner(System.in);
+	       System.out.println("Enter Id: ");
+	       String Id = sc.next();
+	       System.out.println("Enter number of credits: ");
+	       int cs = sc.nextInt();
+	       System.out.println("Enter number of points: ");
+	       int p = sc.nextInt();
+	       Student s = new Student(Id, cs, p);
+	       System.out.println(s);
+	   }
+	}
+
